db45d93b1a2a0c3bb19ec8e8c66479037ac57f19
Stefan Gatterer <stefan.gatterer@gmail.com>
2021-09-21 11:25:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const name1 = [1, 2, 3];
+name1.concat(1)
+console.log(name1)
+
+document.querySelector(".name")
+super.console.log("so"
+
+        ("was ist das")
+
+        document.querySelector
